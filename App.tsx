b1740f92667ba348656b4da3723d4fc4d84905d3
Navdeep Singh <sonynavdeep81@gmail.com>
2026-07-29 20:30:27 +0530

    Wrap app in FontScaleProvider

--- App.tsx
+++ App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AppNavigator } from './src/navigation/AppNavigator';
+import { FontScaleProvider } from './src/context/FontScaleContext';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <StatusBar style="light" />
-      <AppNavigator />
+      <FontScaleProvider>
+        <StatusBar style="light" />
+        <AppNavigator />
+      </FontScaleProvider>
     </SafeAreaProvider>
   );
 }
